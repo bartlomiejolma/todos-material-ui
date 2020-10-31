ac84dd39ec11aa1e1d4b8e2bf6a4730b640c55f4
Bartek Olma <bartek@ucreate.it>
2020-10-31 21:49:03 +0100

    Hello world.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
 function App() {
-  return "Hello world!";
+  return <Button color="primary">Hello World</Button>;
 }
 
 export default App;
