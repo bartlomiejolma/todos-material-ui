68d9143dcf2779261346b367874b6bca18e7bd72
Bartek Olma <bartek@ucreate.it>
2020-11-01 21:07:42 +0100

    Adds basic layout for todos with stub elements.

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,5 +1,28 @@
+import { Grid } from "@material-ui/core";
+import { useState } from "react";
+
+const Todo = ({ todo }) => todo;
+const EmptyTodoList = () => "Empty";
+const GridPadding = () => <Grid item xs={0} sm={2}></Grid>;
+
 const TodoList = () => {
-  return "Todo List";
+  const [todos, setTodos] = useState([1]);
+
+  if (todos.length === 0) {
+    return <EmptyTodoList />;
+  }
+  return (
+    <Grid container>
+      <GridPadding />
+      <Grid item xs={12} sm={8}>
+        {todos.map((todo) => (
+          <Todo todo={todo} />
+        ))}
+      </Grid>
+
+      <GridPadding />
+    </Grid>
+  );
 };
 
 export default TodoList;
