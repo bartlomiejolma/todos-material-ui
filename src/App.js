faea8877dac3a61b383ce745164467f1182bbd23
Bartek Olma <bartek@ucreate.it>
2020-11-01 17:28:44 +0100

    Adds basic appbar.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React from "react";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
-function App() {
+const Content = () => {
   return <Button color="primary">Hello World</Button>;
+};
+function App() {
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5">Todos</Typography>
+        </Toolbar>
+      </AppBar>
+      <Content />
+    </>
+  );
 }
 
 export default App;
