6b8c9c6ca7fbe3723ea11d459400f8019c25622b
Bartek Olma <bartek@ucreate.it>
2020-11-01 22:07:02 +0100

    Fixes eslint error.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button } from "@material-ui/core";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { links, SideMenu } from "./navigation";
 
