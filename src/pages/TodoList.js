76f02916985009997c1168aa13d8144a46092dd0
Bartek Olma <bartek@ucreate.it>
2020-11-01 21:20:41 +0100

    Adds basic view of sample todo.

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,13 +1,35 @@
-import { Grid, IconButton } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Grid,
+  IconButton,
+  Typography,
+} from "@material-ui/core";
 import { NoteAdd } from "@material-ui/icons";
 import { useState } from "react";
 
-const Todo = ({ todo }) => todo;
+const Todo = ({ todo }) => {
+  return (
+    <Card>
+      <CardHeader title={todo.title} />
+      <CardMedia image={todo.image} />
+      <CardContent>
+        <Typography variant="h6">{todo.description}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
 const EmptyTodoList = () => "Empty";
 const GridPadding = () => <Grid item xs={0} sm={2}></Grid>;
 
+const sampleTodo = {
+  title: "foo",
+  description: "lorem lipsum",
+};
 const TodoList = () => {
-  const [todos, setTodos] = useState([1]);
+  const [todos, setTodos] = useState([sampleTodo]);
 
   const addTodoClicked = () => {};
 
@@ -15,7 +37,7 @@ const TodoList = () => {
     <Grid container>
       <GridPadding />
       <Grid item xs={12} sm={8}>
-        <Grid container direction="row">
+        <Grid container direction="column">
           <Grid item>
             {todos.length === 0 && <EmptyTodoList />}
             {todos.map((todo) => (
