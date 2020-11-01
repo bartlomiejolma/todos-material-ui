af6c90e8dab9760864300eec3d827b9c2db92ae2
Bartek Olma <bartek@ucreate.it>
2020-11-01 17:40:42 +0100

    Adds simple drawer.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Button,
+  Divider,
+  Drawer,
   IconButton,
   Toolbar,
   Typography,
 } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+import { Menu, ChevronLeft } from "@material-ui/icons";
 
 const Content = () => {
   return <Button color="primary">Hello World</Button>;
 };
 function App() {
+  const [open, setOpen] = useState(false);
+  const handleDrawerOpen = () => setOpen(true);
+  const handleDrawerClose = () => setOpen(false);
   return (
     <>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start">
-            <MenuIcon />
+          <IconButton edge="start" onClick={handleDrawerOpen}>
+            <Menu />
           </IconButton>
           <Typography variant="h5">Todos</Typography>
         </Toolbar>
       </AppBar>
+      <Drawer variant="persistent" anchor="left" open={open}>
+        <IconButton onClick={handleDrawerClose}>
+          <ChevronLeft />
+          <Divider />
+        </IconButton>
+      </Drawer>
       <Content />
     </>
   );
