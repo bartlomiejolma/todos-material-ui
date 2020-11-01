93b11679b0a3004d45766fab423dde68eed4e364
Bartek Olma <bartek@ucreate.it>
2020-11-01 20:53:25 +0100

    Adds Switch to handle path changes.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Button } from "@material-ui/core";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { links, SideMenu } from "./navigation";
-
-const Content = () => {
-  return <Button color="primary">Hello World</Button>;
-};
 
 function App() {
   return (
     <Router>
       <SideMenu title="Todos" links={links} />
-      <Content />
+      <Switch>
+        {links.map((link) => (
+          <Route exact path={link.path}>{link.component}</Route>
+        ))}
+      </Switch>
     </Router>
   );
 }
