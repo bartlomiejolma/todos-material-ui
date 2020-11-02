fe67f76c166c6975c5c610ab377cd1f49cdb5c1d
Bartek Olma <bartek@ucreate.it>
2020-11-02 20:27:00 +0100

    Wraps app in theme with correct injection order.
    Following instrucitons:
    https://material-ui.com/guides/interoperability/#theme

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { links, SideMenu } from "./navigation";
 
+import { StylesProvider } from "@material-ui/core/styles";
+import { ThemeProvider } from "styled-components";
+
+import {
+  createMuiTheme,
+  ThemeProvider as MuiThemeProvider,
+} from "@material-ui/core/styles";
+
+const customTheme = createMuiTheme({});
+
 function App() {
   return (
-    <Router>
-      <SideMenu title="Todos" links={links} />
-      <Switch>
-        {links.map(({ path, Component }) => (
-          <Route exact path={path}>
-            <Component />
-          </Route>
-        ))}
-      </Switch>
-    </Router>
+    <StylesProvider injectFirst>
+      <MuiThemeProvider theme={customTheme}>
+        <ThemeProvider theme={customTheme}>
+          <Router>
+            <SideMenu title="Todos" links={links} />
+            <Switch>
+              {links.map(({ path, Component }) => (
+                <Route exact path={path}>
+                  <Component />
+                </Route>
+              ))}
+            </Switch>
+          </Router>
+        </ThemeProvider>
+      </MuiThemeProvider>
+    </StylesProvider>
   );
 }
 
