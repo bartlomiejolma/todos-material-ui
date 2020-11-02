d3f1c837e896db44532a084ac87ca60b7cf0af46
Bartek Olma <bartek@ucreate.it>
2020-11-02 21:04:39 +0100

    Adds checkbox icon.

--- src/pages/TodoList/TodoView.js
+++ src/pages/TodoList/TodoView.js
@@ -7,12 +7,24 @@ import {
   Typography,
   IconButton,
 } from "@material-ui/core";
-import { Delete, Search } from "@material-ui/icons";
+import {
+  CheckBox,
+  CheckBoxOutlineBlank,
+  Delete,
+  Search,
+} from "@material-ui/icons";
 
 const TodoView = ({ todo }) => {
   return (
     <Card>
-      <CardHeader title={todo.title} />
+      <CardHeader
+        title={todo.title}
+        action={
+          <IconButton>
+            {todo.completed ? <CheckBox /> : <CheckBoxOutlineBlank />}
+          </IconButton>
+        }
+      />
       <CardMedia image={todo.image} />
       <CardContent>
         <Typography variant="h6">{todo.description}</Typography>
