f9d0081f8c6874def911e4aa149108fd8ebd6d30
Bartek Olma <bartek@ucreate.it>
2020-11-04 21:59:38 +0100

    Extracts initial logic to a setup.

--- cypress/integration/createTodo.spec.js
+++ cypress/integration/createTodo.spec.js
@@ -1,11 +1,14 @@
 describe("Create Todo", () => {
-
-  it("Initially the list of todos is empty", () => {
+  beforeEach(() => {
     cy.visit("/");
     cy.contains("Todos");
     cy.contains("Empty");
+  });
 
-    cy.get(".MuiGrid-grid-xs-12 > :nth-child(1) > :nth-child(2) > .MuiButtonBase-root").click();
+  it("Adds Todos", () => {
+    cy.get(
+      ".MuiGrid-grid-xs-12 > :nth-child(1) > :nth-child(2) > .MuiButtonBase-root"
+    ).click();
     cy.contains("Title").parent().type("Foo");
     cy.contains("Description").parent().type("Bar");
     cy.contains("Due Date").parent().type("2020-09-08");
@@ -17,20 +20,22 @@ describe("Create Todo", () => {
     cy.contains("Foo");
     cy.contains("Bar");
 
-    cy.get(".MuiCard-root").should('have.length', 1)
+    cy.get(".MuiCard-root").should("have.length", 1);
 
-
-    cy.get(".MuiGrid-grid-xs-12 >:nth-child(1) > :nth-child(2) > .MuiButtonBase-root").click();
+    cy.get(
+      ".MuiGrid-grid-xs-12 >:nth-child(1) > :nth-child(2) > .MuiButtonBase-root"
+    ).click();
     cy.contains("Title").parent().type("Foo");
     cy.contains("Description").parent().type("Baz");
     cy.contains("Due Date").parent().type("2020-09-08");
 
     cy.contains("Save").click();
 
-    cy.get(".MuiCard-root").should('have.length', 2)
+    cy.get(".MuiCard-root").should("have.length", 2);
 
-    cy.get(':nth-child(1) > .MuiPaper-root > .MuiCardActions-root > :nth-child(2)').click()
-    cy.get(".MuiCard-root").should('have.length', 1)
-
+    cy.get(
+      ":nth-child(1) > .MuiPaper-root > .MuiCardActions-root > :nth-child(2)"
+    ).click();
+    cy.get(".MuiCard-root").should("have.length", 1);
   });
 });
