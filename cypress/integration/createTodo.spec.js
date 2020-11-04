20d8f4e817c464ff6642a9182a8309a2c3b14a53
Bartek Olma <bartek@ucreate.it>
2020-11-04 22:12:13 +0100

    Adds test for editing todo.

--- cypress/integration/createTodo.spec.js
+++ cypress/integration/createTodo.spec.js
@@ -5,7 +5,7 @@ describe("Create Todo", () => {
     cy.contains("Empty");
   });
 
-  it("Adds Todos", () => {
+  it("Adds Todos and deletes it", () => {
     cy.get(
       ".MuiGrid-grid-xs-12 > :nth-child(1) > :nth-child(2) > .MuiButtonBase-root"
     ).click();
@@ -37,5 +37,32 @@ describe("Create Todo", () => {
       ":nth-child(1) > .MuiPaper-root > .MuiCardActions-root > :nth-child(2)"
     ).click();
     cy.get(".MuiCard-root").should("have.length", 1);
+  });
+
+  it("Can edit Todo", () => {
+    cy.get(
+      ".MuiGrid-grid-xs-12 > :nth-child(1) > :nth-child(2) > .MuiButtonBase-root"
+    ).click();
+    cy.contains("Title").parent().type("Foo");
+    cy.contains("Description").parent().type("Bar");
+    cy.contains("Due Date").parent().type("2020-09-08");
+
+    cy.contains("Save").click();
+
+    cy.contains("Empty").should("not.exist");
+
+    cy.contains("Bar");
+
+    cy.get(
+      ":nth-child(1) > .MuiPaper-root > .MuiCardActions-root > :nth-child(1)"
+    ).click();
+    cy.contains("Title").parent().type("Baz");
+    cy.get("input[name=description]").clear();
+    
+    cy.contains("Save").click();
+
+    cy.contains("FooBaz");
+    cy.contains("Bar").should("not.exist");
+
   });
 });
