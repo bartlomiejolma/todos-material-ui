dc683745953699254beb68ebd51eb67ad6b6f21a
Bartek Olma <bartek@ucreate.it>
2020-11-08 22:02:21 +0100

    Converts column to functional component.

--- src/pages/Kanban/index.js
+++ src/pages/Kanban/index.js
@@ -16,25 +16,23 @@ const TaskList = styled.div`
   padding: 8px;
 `;
 
-class Column extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Title>{this.props.column.title}</Title>
-        <Droppable droppableId={this.props.column.id}>
-          {(provided) => (
-            <TaskList ref={provided.innerRef} {...provided.droppableProps}>
-              {this.props.tasks.map((task, index) => (
-                <Task key={task.id} task={task} index={index} />
-              ))}
-              {provided.placeholder}
-            </TaskList>
-          )}
-        </Droppable>
-      </Container>
-    );
-  }
-}
+const Column = ({column, tasks}) => {
+  return (
+    <Container>
+      <Title>{column.title}</Title>
+      <Droppable droppableId={column.id}>
+        {(provided) => (
+          <TaskList ref={provided.innerRef} {...provided.droppableProps}>
+            {tasks.map((task, index) => (
+              <Task key={task.id} task={task} index={index} />
+            ))}
+            {provided.placeholder}
+          </TaskList>
+        )}
+      </Droppable>
+    </Container>
+  );
+};
 
 const Task = ({ task, index }) => {
   return (
