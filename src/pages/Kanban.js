2af2444f35448cca7bf87fe13d1838fe987b3dc9
Bartek Olma <bartek@ucreate.it>
2020-11-08 21:52:45 +0100

    Adds function which groups tasks by category.

--- src/pages/Kanban.js
+++ src/pages/Kanban.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+const groupTodos = (todos) => {
+  return todos.reduce((groups, todo) => {
+    const group = todo.group || "Backlog";
+    groups[group] = groups[group] || [];
+    groups[group].push(todo);
+    return groups;
+  }, {});
+};
 const Kanban = () => {
   const todos = useSelector((state) => state.todos);
 
