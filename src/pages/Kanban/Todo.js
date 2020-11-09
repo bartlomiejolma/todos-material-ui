a75d9793f689eed8a3ce395f272abc595b876912
Bartek Olma <bartek@ucreate.it>
2020-11-09 21:59:55 +0100

    Styles Todo.

--- src/pages/Kanban/Todo.js
+++ src/pages/Kanban/Todo.js
@@ -2,23 +2,27 @@ import React from "react";
 
 import { Draggable } from "react-beautiful-dnd";
 
-import { Card, CardContent, Typography } from "@material-ui/core";
+import { Card, CardContent, Grid, Typography } from "@material-ui/core";
 
 const Todo = ({ todo, index }) => {
   return (
     <Draggable draggableId={todo.id.toString()} index={index}>
       {(provided) => (
-        <Card
+        <Grid
+          item
+          xs={12}
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              {todo.title}
-            </Typography>
-          </CardContent>
-        </Card>
+          <Card>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                {todo.title}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
       )}
     </Draggable>
   );
