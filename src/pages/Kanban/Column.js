d5d25232d8666ed440d2eec30a73cdb7a154aec0
Bartek Olma <bartek@ucreate.it>
2020-11-09 22:00:22 +0100

    Styles column.

--- src/pages/Kanban/Column.js
+++ src/pages/Kanban/Column.js
@@ -3,29 +3,39 @@ import React from "react";
 import styled from "styled-components";
 import { Droppable } from "react-beautiful-dnd";
 import Todo from "./Todo";
-import { Paper, Typography } from "@material-ui/core";
+import { Grid, Paper, Typography } from "@material-ui/core";
 
-const TaskList = styled.div`
+const StyledPaper = styled(Paper)`
   padding: 8px;
+  min-height: 500px;
+  margin: 20px;
 `;
 
 const Column = ({ column, tasks }) => {
   return (
-    <Paper elevation={3}>
-      <Typography variant="h5" component="h3">
-        {column.title}
-      </Typography>
-      <Droppable droppableId={column.id}>
-        {(provided) => (
-          <TaskList ref={provided.innerRef} {...provided.droppableProps}>
+    <Droppable droppableId={column.id}>
+      {(provided) => (
+        <Grid
+          item
+          xs={4}
+          container
+          spacing={3}
+          direction="column"
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+        >
+          <StyledPaper elevation={3}>
+            <Typography variant="h5" component="h3">
+              {column.title}
+            </Typography>
             {tasks.map((todo, index) => (
               <Todo key={todo.id} todo={todo} index={index} />
             ))}
             {provided.placeholder}
-          </TaskList>
-        )}
-      </Droppable>
-    </Paper>
+          </StyledPaper>
+        </Grid>
+      )}
+    </Droppable>
   );
 };
 
