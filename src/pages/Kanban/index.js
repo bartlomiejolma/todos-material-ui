567920b97c5a76e0e628c1dc043df0404f4c399c
Bartek Olma <bartek@ucreate.it>
2020-11-09 22:07:02 +0100

    Adds padding to the lists.

--- src/pages/Kanban/index.js
+++ src/pages/Kanban/index.js
@@ -5,6 +5,7 @@ import { DragDropContext } from "react-beautiful-dnd";
 import { changeGroupOfTodo, changeIndexOfTodo } from "../../store/todos";
 import Column from "./Column";
 import { Grid } from "@material-ui/core";
+import styled from "styled-components";
 
 const groupTodos = (todos) => {
   return todos.reduce(
@@ -17,6 +18,11 @@ const groupTodos = (todos) => {
     { Backlog: [], "In progress": [], Done: [] }
   );
 };
+
+const GridContainer = styled(Grid)`
+  padding: 10px
+`;
+
 const Kanban = () => {
   const todos = useSelector((state) => state.todos) || [];
   const dispatch = useDispatch();
@@ -33,13 +39,13 @@ const Kanban = () => {
     dispatch(changeIndexOfTodo({ id: +draggableId, index }));
   };
   return (
-    <Grid container spacing={4}>
+    <GridContainer container spacing={4}>
       <DragDropContext onDragEnd={onDragEnd}>
         {Object.entries(groupedTodos).map(([group, tasks]) => (
           <Column column={{ title: group, id: group }} tasks={tasks} />
         ))}
       </DragDropContext>
-    </Grid>
+    </GridContainer>
   );
 };
 
