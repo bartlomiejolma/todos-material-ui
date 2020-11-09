14aba98301f444cd1f4a6abe4f8fea7756b544ff
Bartek Olma <bartek@ucreate.it>
2020-11-09 21:19:31 +0100

    Extracts column to a file.

---
+++ src/pages/Kanban/Column.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import styled from "styled-components";
+import { Droppable } from "react-beautiful-dnd";
+import Todo from "./Todo";
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+`;
+
+const Column = ({ column, tasks }) => {
+  return (
+    <Container>
+      <Title>{column.title}</Title>
+      <Droppable droppableId={column.id}>
+        {(provided) => (
+          <TaskList ref={provided.innerRef} {...provided.droppableProps}>
+            {tasks.map((todo, index) => (
+              <Todo key={todo.id} todo={todo} index={index} />
+            ))}
+            {provided.placeholder}
+          </TaskList>
+        )}
+      </Droppable>
+    </Container>
+  );
+};
+
+export default Column;
--- src/pages/Kanban/index.js
+++ src/pages/Kanban/index.js
@@ -1,42 +1,9 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import styled from "styled-components";
-import { Droppable, Draggable, DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext } from "react-beautiful-dnd";
 import { changeGroupOfTodo, changeIndexOfTodo } from "../../store/todos";
-import Todo from "./Todo";
-
-const Container = styled.div`
-  margin: 8px;
-  border: 1px solid lightgrey;
-  border-radius: 2px;
-`;
-const Title = styled.h3`
-  padding: 8px;
-`;
-const TaskList = styled.div`
-  padding: 8px;
-`;
-
-const Column = ({ column, tasks }) => {
-  return (
-    <Container>
-      <Title>{column.title}</Title>
-      <Droppable droppableId={column.id}>
-        {(provided) => (
-          <TaskList ref={provided.innerRef} {...provided.droppableProps}>
-            {tasks.map((todo, index) => (
-              <Todo key={todo.id} todo={todo} index={index} />
-            ))}
-            {provided.placeholder}
-          </TaskList>
-        )}
-      </Droppable>
-    </Container>
-  );
-};
-
-
+import Column from "./Column";
 
 const groupTodos = (todos) => {
   return todos.reduce(
