7d4ac2016b829968a656ef8302b6bb159d78af45
Bartek Olma <bartek@ucreate.it>
2020-11-09 22:07:42 +0100

    Correctly adds spacing to the todos.

--- src/pages/Kanban/Column.js
+++ src/pages/Kanban/Column.js
@@ -8,30 +8,23 @@ import { Grid, Paper, Typography } from "@material-ui/core";
 const StyledPaper = styled(Paper)`
   padding: 8px;
   min-height: 500px;
-  margin: 20px;
 `;
 
 const Column = ({ column, tasks }) => {
   return (
     <Droppable droppableId={column.id}>
       {(provided) => (
-        <Grid
-          item
-          xs={4}
-          container
-          spacing={3}
-          direction="column"
-          ref={provided.innerRef}
-          {...provided.droppableProps}
-        >
+        <Grid item xs={4} ref={provided.innerRef} {...provided.droppableProps}>
           <StyledPaper elevation={3}>
             <Typography variant="h5" component="h3">
               {column.title}
             </Typography>
-            {tasks.map((todo, index) => (
-              <Todo key={todo.id} todo={todo} index={index} />
-            ))}
-            {provided.placeholder}
+            <Grid container spacing={1}>
+              {tasks.map((todo, index) => (
+                <Todo key={todo.id} todo={todo} index={index} />
+              ))}
+              {provided.placeholder}
+            </Grid>
           </StyledPaper>
         </Grid>
       )}
