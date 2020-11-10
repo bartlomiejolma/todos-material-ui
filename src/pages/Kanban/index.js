2cf6484e16b0e9d6dfab2c9d6ccea4afcc3e8836
Bartek Olma <bartek@ucreate.it>
2020-11-10 21:41:10 +0100

    Shows todos in kanban with new structure.

--- src/pages/Kanban/index.js
+++ src/pages/Kanban/index.js
@@ -2,46 +2,37 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { DragDropContext } from "react-beautiful-dnd";
-import { changeGroupOfTodo, changeIndexOfTodo } from "../../store/todos";
+import { moveTodo } from "../../store/todos";
 import Column from "./Column";
 import { Grid } from "@material-ui/core";
 import styled from "styled-components";
-
-const groupTodos = (todos) => {
-  return todos.reduce(
-    (groups, todo) => {
-      const group = todo.group || "Backlog";
-      groups[group] = groups[group] || [];
-      groups[group].push(todo);
-      return groups;
-    },
-    { Backlog: [], "In progress": [], Done: [] }
-  );
-};
+import { createSelector } from "@reduxjs/toolkit";
 
 const GridContainer = styled(Grid)`
-  padding: 10px
+  padding: 10px;
 `;
 
+const getTodos = (state) => state.todos;
+const getTodosWithDefaultGroups = createSelector([getTodos], (todos) => {
+  const defaultGroups = ["backlog", "in_progress", "done"];
+  return defaultGroups.map((groupName) => [groupName, todos[groupName] || []]);
+});
+
 const Kanban = () => {
-  const todos = useSelector((state) => state.todos) || [];
+  const groupedTodos = useSelector(getTodosWithDefaultGroups);
   const dispatch = useDispatch();
 
-  const groupedTodos = groupTodos(todos);
   const onDragEnd = (result) => {
     const { draggableId, destination } = result;
-    console.log(result);
     if (!destination) {
       return;
     }
     const { droppableId, index } = destination;
-    dispatch(changeGroupOfTodo({ id: +draggableId, group: droppableId }));
-    dispatch(changeIndexOfTodo({ id: +draggableId, index }));
   };
   return (
     <GridContainer container spacing={4}>
       <DragDropContext onDragEnd={onDragEnd}>
-        {Object.entries(groupedTodos).map(([group, tasks]) => (
+        {groupedTodos.map(([group, tasks]) => (
           <Column column={{ title: group, id: group }} tasks={tasks} />
         ))}
       </DragDropContext>
