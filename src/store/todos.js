c42b8f02eb05e532a332b7e2fd3b29f7c9c939f5
Bartek Olma <bartek@ucreate.it>
2020-11-10 21:27:07 +0100

    Changes content of todo in group.

--- src/store/todos.js
+++ src/store/todos.js
@@ -1,25 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { getTodosFromLocalStorage } from "../useLocalStorageTodos";
 
+const findAndModifyTodo = (nextTodo, todo) => {
+  if (nextTodo.id === todo.id) {
+    return todo;
+  }
+  return nextTodo;
+};
 const todoSlice = createSlice({
   name: "todos",
   initialState: getTodosFromLocalStorage() || {},
   reducers: {
     addTodo(state, action) {
-      const { todo, category="backlog" } = action.payload;
+      const { todo, category = "backlog" } = action.payload;
       todo.id = Math.floor(Math.random() * 1000);
-      const group = state[category] || []
-      group.push(todo)
-      return {...state, [category]: group}
+      const group = state[category] || [];
+      group.push(todo);
+      return { ...state, [category]: group };
     },
     changeTodo: (state, action) => {
       const { todo } = action.payload;
-      return state.map((nextTodo) => {
-        if (nextTodo.id === todo.id) {
-          return todo;
-        }
-        return nextTodo;
-      });
+      return Object.entries(state).reduce(
+        (acc, [group, groupTodos]) => ({
+          ...acc,
+          [group]: groupTodos.map((nextTodo) =>
+            findAndModifyTodo(nextTodo, todo)
+          ),
+        }),
+        {}
+      );
     },
     deleteTodo: (state, action) => {
       const { todo } = action.payload;
@@ -27,7 +36,6 @@ const todoSlice = createSlice({
     },
     changeGroupOfTodo: (state, action) => {
       const { id, group } = action.payload;
-      console.log(action)
       return state.map((todo) => {
         if (todo.id === id) {
           return { ...todo, group };
