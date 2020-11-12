fee062a55aa1aea715c175ac07daacd359650284
Bartek Olma <bartek@ucreate.it>
2020-11-12 20:31:04 +0100

    Fixes formatting of the single column.

--- src/pages/Kanban/Column.js
+++ src/pages/Kanban/Column.js
@@ -8,28 +8,35 @@ import { titleize } from "inflected";
 
 const StyledPaper = styled(Paper)`
   padding: 8px;
+`;
+const DropZone = styled(Grid)`
   min-height: 500px;
 `;
 
 const Column = ({ column, tasks }) => {
   return (
-    <Droppable droppableId={column.id}>
-      {(provided) => (
-        <Grid item xs={4} ref={provided.innerRef} {...provided.droppableProps}>
-          <StyledPaper elevation={3}>
-            <Typography variant="h5" component="h3">
-              {titleize(column.title)}
-            </Typography>
-            <Grid container spacing={1}>
+    <Grid item xs={4}>
+      <StyledPaper elevation={3}>
+        <Typography variant="h5" component="h3">
+          {titleize(column.title)}
+        </Typography>
+        <Droppable droppableId={column.id}>
+          {(provided) => (
+            <DropZone
+              container
+              spacing={1}
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
               {tasks.map((todo, index) => (
                 <Todo key={todo.id} todo={todo} index={index} />
               ))}
               {provided.placeholder}
-            </Grid>
-          </StyledPaper>
-        </Grid>
-      )}
-    </Droppable>
+            </DropZone>
+          )}
+        </Droppable>
+      </StyledPaper>
+    </Grid>
   );
 };
 
